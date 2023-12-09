e8b3f328deec78ac48d250ea3e4f2c2d0080edb0
hbayoud <hugo.bayoud@gmail.com>
2023-12-09 22:07:14 +0100

    Add alt tag in Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { poppins } from '@/app/ui/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Lusitana } from 'next/font/google';
+
+export const poppins = Poppins({ weight: '700', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
